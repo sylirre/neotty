a3a74e65c7df52a9f705735e91bd850075b260e2
Sylirre <107305601+sylirre@users.noreply.github.com>
2024-02-01 17:12:28 +0200

    qemu: update zlib

--- external/libqemu-jni/packages/zlib/build.sh
+++ external/libqemu-jni/packages/zlib/build.sh
@@ -1,8 +1,12 @@
-PACKAGE_VERSION="1.3"
+PACKAGE_VERSION="1.3.1"
 PACKAGE_SRCURL="https://www.zlib.net/zlib-$PACKAGE_VERSION.tar.xz"
-PACKAGE_SHA256="8a9ba2898e1d0d774eca6ba5b4627a11e5588ba85c8851336eb38de4683050a7"
+PACKAGE_SHA256="38ef96b8dfe510d42707d9c781877914792541133e1870841463bfa73f883e32"
 
 builder_step_configure() {
+	if [ "$PACKAGE_TARGET_ARCH" = "aarch64" ]; then
+		CFLAGS+=" -march=armv8-a+crc"
+	fi
+
 	CFLAGS+=" $CPPFLAGS -fPIC"
 	LDFLAGS+=" -fPIC"
 	"$PACKAGE_SRCDIR"/configure \
